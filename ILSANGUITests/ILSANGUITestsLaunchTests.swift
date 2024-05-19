7eae4c89507eb2f16044d455731ffbc557a89d93
radin <dlwlsgml8847@gmail.com>
2024-05-20 00:15:19 +0900

    [Chore/#9] 프로젝트 생성

---
+++ ILSANGUITests/ILSANGUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ILSANGUITestsLaunchTests.swift
+//  ILSANGUITests
+//
+//  Created by Lee Jinhee on 5/19/24.
+//
+
+import XCTest
+
+final class ILSANGUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
