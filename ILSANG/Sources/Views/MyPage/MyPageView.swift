48b525f07416f806de32e3a270c7c48f1e242729
HIBAR97 <hibar9710@gmail.com>
2024-05-22 19:47:10 +0900

    [Design/#15] 기본 틀 작성

--- ILSANG/Sources/Views/MyPage/MyPageView.swift
+++ ILSANG/Sources/Views/MyPage/MyPageView.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct MyPageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            
+            //프로필 제목
+            HStack {
+                Text("내 프로필")
+                Spacer()
+                Image(systemName: "gearshape")
+            }
+            
+            //개인 프로필
+            
+            //Segmenet
+            
+            //퀘스트 List
+            
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
