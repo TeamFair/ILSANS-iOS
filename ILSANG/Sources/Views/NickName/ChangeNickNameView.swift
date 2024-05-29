384e2e06354e7b20e656a9c5db39583ab25229d4
HIBAR97 <hibar9710@gmail.com>
2024-05-29 23:03:53 +0900

    [Feat/#21] 닉네임 변경 뷰 기능 구현

--- ILSANG/Sources/Views/NickName/ChangeNickNameView.swift
+++ ILSANG/Sources/Views/NickName/ChangeNickNameView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ChangeNickNameView: View {
     
     @State private var name: String = ""
+    @State private var isSame : Bool = false
     
     var body: some View {
         NavigationView {
@@ -24,17 +25,32 @@ struct ChangeNickNameView: View {
                         .frame(maxWidth: .infinity,maxHeight: 50)
                         .background(.gray100)
                         .cornerRadius(12)
+                        //틀렸을때 테두리
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(isSame ? Color.red : Color.clear, lineWidth: 3)
+                                .cornerRadius(12)
+                        )
                     
-                    HStack (alignment: .center){
                         TextField("닉네임을 입력하세요", text: $name)
-                    }
+                    
                     .padding(14)
                 }
                 
                 Text("입력하신 닉네임은 이미 사용중이에요.\n다른 닉네임을 입력해주세요")
+                    .opacity(isSame ? 1 : 0)
                     .foregroundColor(.red)
                 
                 Spacer()
+                
+                Text("변경 완료")
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity ,maxHeight: 50)
+                    .background(Color.accentColor)
+                    .cornerRadius(12)
+                    .disabled(!isSame)
+                    //MARK: 디버깅용 기능
+                    .onTapGesture { isSame.toggle() }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
