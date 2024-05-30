ae6563a962d84746ec4fbb19f7a8b20e5aff1b03
HIBAR97 <hibar9710@gmail.com>
2024-05-30 16:11:53 +0900

    Design/#21] 약관 및 정책 뷰 디자인 구현

--- ILSANG/Sources/Views/Setting/TermsAndPolicyView.swift
+++ ILSANG/Sources/Views/Setting/TermsAndPolicyView.swift
@@ -6,10 +6,32 @@
 //
 
 import SwiftUI
+import PDFKit
 
 struct TermsAndPolicyView: View {
+    
+    let pdfUrl = Bundle.main.url(forResource: "24.06_일상_이용약관", withExtension: "pdf")!
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            PDFKitView(url: pdfUrl)
+        }
+        .navigationTitle("약관 및 정책")
+    }
+}
+
+//내부 PDF를 뷰로 변경
+struct PDFKitView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: UIViewRepresentableContext<PDFKitView>) -> PDFView {
+        let pdfView = PDFView()
+        pdfView.document = PDFDocument(url: self.url)
+        pdfView.autoScales = true
+        return pdfView
+    }
+    
+    func updateUIView(_ uiView: PDFView, context: UIViewRepresentableContext<PDFKitView>) {
     }
 }
 
