f0693f775f645356c49c48dbe46fdca91612e033
radin <dlwlsgml8847@gmail.com>
2024-06-09 20:30:04 +0900

    [Design/#34] 탭바 포지셔닝 방식 수정

--- ILSANG/Sources/Views/ILSANGApp.swift
+++ ILSANG/Sources/Views/ILSANGApp.swift
@@ -23,6 +23,7 @@ struct ILSANGApp: App {
         let appearance = UITabBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.shadowColor = UIColor(.grayDD)
+        appearance.stackedItemPositioning = .centered
         UITabBar.appearance().scrollEdgeAppearance = appearance
     }
 }
