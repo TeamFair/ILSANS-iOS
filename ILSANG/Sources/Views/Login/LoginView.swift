07e95aebc8a89cfa0694b0b56e25e81dabc62b46
radin <dlwlsgml8847@gmail.com>
2024-06-09 22:04:54 +0900

    [Design/#38] 로그인뷰 이미지 배경 변경 및 로고 추가, 구글로그인버튼 배경 설정

--- ILSANG/Sources/Views/Login/LoginView.swift
+++ ILSANG/Sources/Views/Login/LoginView.swift
@@ -10,9 +10,7 @@ import SwiftUI
 struct LoginView: View {
     
     @Binding var isLogin : Bool
-    
-    @State var isVisitor: Bool = false
-    
+        
     var kakaoButtonAction: () -> Void {
         {
             //MARK: 로그인 기능 구현
@@ -26,15 +24,8 @@ struct LoginView: View {
     }
     
     var body: some View {
-        VStack(spacing: 0) {
-            Image("MainLogo")
-                .resizable()
-                .frame(width: 80, height: 80)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 124, height: 124)
-                .background(Color.accent)
-                .cornerRadius(36)
-                .padding(.bottom, 128)
+        VStack(spacing: 84) {
+            Image(.logo)
             
             VStack(spacing: 16) {
                 KakaoLoginButtonView(buttonAction: kakaoButtonAction)
@@ -47,16 +38,14 @@ struct LoginView: View {
                 Text("테스트 로그인")
                     .onTapGesture { isLogin.toggle() }
             }
-            .padding(.bottom, 25)
         }
-        .frame(maxHeight: .infinity)
-        .ignoresSafeArea()
-        .background(Color.white)
-        .accentColor(.black)
+        .padding(.top, 90)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.primaryPurple)
         .navigationBarBackButtonHidden()
-        .navigationDestination(isPresented: $isVisitor) {
-            //TabbarView(isVisitor: $isVisitor)
-        }
     }
 }
 
+#Preview {
+    LoginView(isLogin: .constant(false))
+}
