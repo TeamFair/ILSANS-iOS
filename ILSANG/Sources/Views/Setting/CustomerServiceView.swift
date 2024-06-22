1621322f92562f4785de5d7a088efd30aaa9cbbc
HIBAR97 <hibar9710@gmail.com>
2024-06-22 15:31:26 +0900

    [Feat/#51] 고객센터 인스타 연결

--- ILSANG/Sources/Views/Setting/CustomerServiceView.swift
+++ ILSANG/Sources/Views/Setting/CustomerServiceView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct CustomerServiceView: View {
     @Environment(\.dismiss) var dismiss
@@ -16,20 +17,44 @@ struct CustomerServiceView: View {
                 dismiss()
             }
             
-            List {
-                NavigationLink {
-                    //
-                } label: {
-                    Text("자주 묻는 질문")
-                }
+            HStack {
+                Text("인스타그램")
+                    .font(.system(size: 17).bold())
+                    .foregroundColor(.gray400)
                 
-                Text("이메일주소: findtastyquest@gmail.com")
+                Spacer()
+                
+                Text("1sang.official")
+                    .underline()
+                    .font(.system(size: 17))
+                    .foregroundColor(.gray200)
             }
-            .listRowBackground(Color.white)
-            .listStyle(.plain)
+            .padding(.horizontal, 25)
+            .padding(.vertical, 26)
+            .background(.white)
+            .onTapGesture {
+                openInsragram()
+            }
+            
+            Spacer()
+            
         }
         .frame(maxWidth: .infinity)
+        //MARK: 색상 변경시 수정
+        .background(Color.gray100)
         .navigationBarBackButtonHidden()
+    }
+}
+
+private func openInsragram() {
+    //고객센터 인스타 URL
+    let appURL = URL(string: "instagram://user?username=1sang.official")!
+    let webURL = URL(string: "https://www.instagram.com/1sang.official?igsh=MXM3YTB1ZXp5aDFzOQ%3D%3D&utm_source=qr")!
+    
+    if UIApplication.shared.canOpenURL(appURL) {
+          UIApplication.shared.open(appURL, options: [:], completionHandler: nil)
+    } else {
+        UIApplication.shared.open(webURL)
     }
 }
 
