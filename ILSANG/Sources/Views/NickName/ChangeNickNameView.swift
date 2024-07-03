72081588586c588392f3cc1eb97796a3a17756fa
HIBAR97 <hibar9710@gmail.com>
2024-07-03 17:35:23 +0900

    [Design/71] 닉네임뷰 버튼 중복 제거

--- ILSANG/Sources/Views/NickName/ChangeNickNameView.swift
+++ ILSANG/Sources/Views/NickName/ChangeNickNameView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ChangeNickNameView: View {
+    
     @Environment(\.dismiss) var dismiss
     
     @State private var name: String = ""
@@ -16,97 +17,91 @@ struct ChangeNickNameView: View {
     @State private var showAlert : Bool = false
     
     var body: some View {
-        ZStack {
-            VStack {
-                NavigationTitleView(title: "정보 수정") {
-                    if name.isEmpty {
-                        dismiss()
-                    } else {
-                        showAlert.toggle()
-                    }
-                }
-                
-                VStack (alignment: .leading){
-                    //설명
-                    Text("새로운 닉네임을 입력하세요")
-                    
-                    //닉네임 입력란
-                    ZStack {
-                        Rectangle()
-                            .foregroundColor(.clear)
-                            .frame(maxWidth: .infinity,maxHeight: 50)
-                            .background(.gray100)
-                            .cornerRadius(12)
-                        //틀렸을때 테두리
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 12)
-                                    .stroke(isSame ? Color.red : Color.clear, lineWidth: 3)
-                                    .cornerRadius(12)
-                            )
-                        
-                        TextField("닉네임을 입력하세요", text: $name)
-                            .padding(14)
-                            .onChange(of: name) { _ in
-                                Task {
-                                    //중복되는 아이디가 있거나 서버 연결에 문제 있을 경우
-                                    if await UserNetwork().putUser(nickname: name) {
-                                        isSame = true
-                                    } else {
-                                        //중복되는 아이디가 없을 경우
-                                        isSame = false
-                                    }
-                                }
-                            }
-                        
-                        Text("입력하신 닉네임은 이미 사용중이에요.\n다른 닉네임을 입력해주세요")
-                            .opacity(isSame ? 1 : 0)
-                            .foregroundColor(.red)
-                        
-                        Spacer()
-                        
-                        Text("변경 완료")
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity ,maxHeight: 50)
-                            .background(Color.accentColor)
-                            .cornerRadius(12)
-                            .disabled(!isSame)
-                        //MARK: 디버깅용 기능
-                            .onTapGesture { isSame.toggle() }
-                    }
-                    .navigationBarBackButtonHidden()
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .padding(20)
-                }
-                
-                Text("변경 완료")
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .infinity ,maxHeight: 50)
-                    .background(Color.accentColor)
-                    .cornerRadius(12)
-                    .onTapGesture {
-                        showAlert.toggle()
-                    }
-                    .disabled(!isSame && name.isEmpty)
+        NavigationTitleView(title: "정보 수정") {
+            if name.isEmpty {
+                dismiss()
+            } else {
+                showAlert.toggle()
             }
-            .navigationBarBackButtonHidden()
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .padding(20)
-            .alert(isPresented: $showAlert) {
-                let firstButton = Alert.Button.default(Text("확인")) {
+        }
+        
+        VStack (alignment: .leading){
+            //설명
+            Text("새로운 닉네임을 입력하세요")
+            
+            //닉네임 입력란
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(maxWidth: .infinity,maxHeight: 50)
+                    .background(.gray100)
+                    .cornerRadius(12)
+                //틀렸을때 테두리
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(isSame ? Color.red : Color.clear, lineWidth: 3)
+                            .cornerRadius(12)
+                    )
+                
+                TextField("닉네임을 입력하세요", text: $name)
+                    .padding(14)
+                    .onChange(of: name) { _ in
+                        Task {
+                            //중복되는 아이디가 있거나 서버 연결에 문제 있을 경우
+                            if await UserNetwork().putUser(nickname: name) {
+                                isSame = true
+                            } else {
+                                //중복되는 아이디가 없을 경우
+                                isSame = false
+                            }
+                        }
+                    }
+            }
+            
+            Text("입력하신 닉네임은 이미 사용중이에요.\n다른 닉네임을 입력해주세요")
+                .opacity(isSame ? 1 : 0)
+                .foregroundColor(.red)
+            
+            Spacer()
+            
+            
+            Text("변경 완료")
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity ,maxHeight: 50)
+                .background(Color.accentColor)
+                .cornerRadius(12)
+                .onTapGesture {
                     Task {
-                        //닉네임 변경 시도
+                        //중복되는 아이디가 있거나 서버 연결에 문제 있을 경우
                         if await UserNetwork().putUser(nickname: name) {
-                            showAlert = false
+                            isSame = true
+                            //MARK: 이름 변경되었을때 뷰 확인
+                        } else {
+                            //중복되는 아이디가 없을 경우
+                            isSame = false
                         }
                     }
                 }
-                let secondButton = Alert.Button.cancel(Text("취소")) {
-                    showAlert = false
+                .disabled(!isSame && name.isEmpty)
+        }
+        .navigationBarBackButtonHidden()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(20)
+        .alert(isPresented: $showAlert) {
+            let firstButton = Alert.Button.default(Text("확인")) {
+                Task {
+                    //닉네임 변경 시도
+                    if await UserNetwork().putUser(nickname: name) {
+                        showAlert = false
+                    }
                 }
-                return Alert(title: Text("닉네임 변경을 취소할까요?"),
-                             message: Text("변경을 완료하지 않으면\n프로필이 저장되지 않습니다."),
-                             primaryButton: firstButton, secondaryButton: secondButton)
             }
+            let secondButton = Alert.Button.cancel(Text("취소")) {
+                showAlert = false
+            }
+            return Alert(title: Text("닉네임 변경을 취소할까요?"),
+                         message: Text("변경을 완료하지 않으면\n프로필이 저장되지 않습니다."),
+                         primaryButton: firstButton, secondaryButton: secondButton)
         }
     }
 }
