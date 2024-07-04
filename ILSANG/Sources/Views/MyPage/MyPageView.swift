289680a28c0420877c1d7946da8c5ff3ba987ec9
radin <dlwlsgml8847@gmail.com>
2024-07-05 03:30:44 +0900

    [Design/#75] MyPageView 하위뷰 디테일 수정 및 오타 수정

--- ILSANG/Sources/Views/MyPage/MyPageView.swift
+++ ILSANG/Sources/Views/MyPage/MyPageView.swift
@@ -14,38 +14,36 @@ struct MyPageView: View {
         QuestDetail(questTitle: "Title2", questImage: nil, questDetail: "Detail2", questLike: 100, questDate: "2023.03.06",questXP: 3,status: .QUEST)
        ]
     
-    @State var segmenetSelect = 0
-    @State private var isSettingsViewActive = false
+    @State var segmentSelect = 0
     
     var body: some View {
         NavigationView {
-            VStack {
-                
-                //프로필 제목
+            VStack(spacing: 0) {
+                // 프로필 제목
                 HStack {
                     Text("내 프로필")
                         .font(.system(size: 21))
                         .fontWeight(.bold)
                         .foregroundColor(.gray500)
                     Spacer()
-                    
                     NavigationLink(destination: SettingView()) {
                         Image("Setting")
-                            .foregroundColor(.gray300)
+                            .foregroundColor(.gray500)
                     }
                 }
+                .frame(height: 50)
+                .padding(.bottom, 5)
                 
-                //개인 프로필
+                // 개인 프로필
                 MyPageProfile()
                 
-                //Segmenet
-                MyPageSegment(selectedIndex: $segmenetSelect)
+                // 퀘스트/활동/뱃지 세그먼트
+                MyPageSegment(selectedIndex: $segmentSelect)
                 
-                //퀘스트 List
-                MyPageList(data: $testData, segmenetSelect: $segmenetSelect)
+                // 퀘스트/활동/뱃지 리스트
+                MyPageList(data: $testData, segmentSelect: $segmentSelect)
             }
-            .padding(21)
-            //MARK: 추후 삭제
+            .padding(.horizontal, 20)
             .background(Color.background)
         }
     }
