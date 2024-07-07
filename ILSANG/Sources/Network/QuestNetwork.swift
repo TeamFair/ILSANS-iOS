4d00bd8bc8dac3de60b86989359aa2220eaa374c
HIBAR97 <hibar9710@gmail.com>
2024-07-07 13:35:04 +0900

    [Feat/#62] 퀘스트 URL 1개로 수정

--- ILSANG/Sources/Network/QuestNetwork.swift
+++ ILSANG/Sources/Network/QuestNetwork.swift
@@ -9,23 +9,20 @@ import Foundation
 import Alamofire
 
 final class QuestNetwork {
-    private let uncompletedUrl: String
-    private let completedUrl: String
     
-    init(uncompletedUrl: String = APIManager.makeURL(CustomerTarget(path: "")), completedUrl: String = APIManager.makeURL(CustomerTarget(path: ""))) {
-        self.uncompletedUrl = uncompletedUrl
-        self.completedUrl = completedUrl
+    private let questUrl: String
+    
+    init(questUrl: String = APIManager.makeURL(CustomerTarget(path: ""))) {
+        self.questUrl = questUrl
     }
     
-    //미완료 Quest
-    func getUncompletedQuest(page: Int) async ->  Result<AWSQuest, Error> {
+    func getUncompletedQuest(page: Int) async -> Result<AWSQuest, Error> {
         let parameters: Parameters = ["page": page, "size": "10"]
-        return await Network.requestData(url: uncompletedUrl+"uncompletedQuest", method: .get, parameters: parameters, withToken: true)
+        return await Network.requestData(url: questUrl+"uncompletedQuest", method: .get, parameters: parameters, withToken: true)
     }
     
-    //완료 Quest
-    func getCompletedQuest(page: Int) async ->  Result<AWSQuest, Error> {
+    func getCompletedQuest(page: Int) async -> Result<AWSQuest, Error> {
         let parameters: Parameters = ["page": page, "size": "10"]
-        return await Network.requestData(url: completedUrl+"completedQuest", method: .get, parameters: parameters, withToken: true)
+        return await Network.requestData(url: questUrl+"completedQuest", method: .get, parameters: parameters, withToken: true)
     }
 }
