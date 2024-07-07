1176114fb472e4c626f00719a84fb79aa4266f91
HIBAR97 <hibar9710@gmail.com>
2024-07-07 14:46:33 +0900

    [Feat/#74] XP Api model 수정

--- ILSANG/Sources/Views/MyPage/MypageViewModel.swift
+++ ILSANG/Sources/Views/MyPage/MypageViewModel.swift
@@ -9,12 +9,15 @@ import Foundation
 
 class MypageViewModel: ObservableObject {
     @Published var userData: User?
+    @Published var questXp: [XP]?
     
     private let userNetwork: UserNetwork
+    private let xpNetwork: XPNetwork
     
-    init(userData: User? = nil, userNetwork: UserNetwork) {
+    init(userData: User? = nil, userNetwork: UserNetwork, xpNetwork: XPNetwork) {
         self.userData = userData
         self.userNetwork = userNetwork
+        self.xpNetwork = xpNetwork
     }
     
     @MainActor
@@ -28,6 +31,20 @@ class MypageViewModel: ObservableObject {
             
         case .failure:
             self.userData = nil
+        }
+    }
+    
+    @MainActor
+    func getxpLog(userId: String, title: String, page: Int) async {
+        let res = await xpNetwork.getXP(userId: userId, title: title, page: page, size: 10)
+        
+        switch res {
+        case .success(let model):
+            self.questXp = [model]
+            Log(questXp)
+            
+        case .failure:
+            self.questXp = nil
         }
     }
 }
