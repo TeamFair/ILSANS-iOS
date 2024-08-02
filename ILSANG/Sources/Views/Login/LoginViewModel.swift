b6a48312e9264596b8e8ca82621b0404218114bf
radin <dlwlsgml8847@gmail.com>
2024-08-03 00:55:55 +0900

    [Feat/#99] DEBUG일 때만 카카오, 구글 로그인 버튼 활성화하도록 구현

--- ILSANG/Sources/Views/Login/LoginViewModel.swift
+++ ILSANG/Sources/Views/Login/LoginViewModel.swift
@@ -18,11 +18,13 @@ class LoginViewModel: ObservableObject {
         //TODO: 로그인 기능 구현
     }
     
+#if DEBUG
     func testLogin() {
         Task {
             await UserService.shared.loginWithTest()
         }
     }
+#endif
     
     func loginWithApple(credential: ASAuthorizationCredential) {
         guard let credential = credential as? ASAuthorizationAppleIDCredential else { return }
