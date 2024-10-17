4ecd772d9f663c2e62e5704e9a8d8bbc76d00ba8
radin <dlwlsgml8847@gmail.com>
2024-10-17 16:08:31 +0900

    [Feat/#165] Challenge 모델에 CodingKeys 추가 (questImageId 대응)

--- ILSANG/Sources/Model/Challenge.swift
+++ ILSANG/Sources/Model/Challenge.swift
@@ -17,6 +17,17 @@ struct Challenge: Codable {
     let createdAt: String
     let likeCnt, hateCnt: Int
     
+    enum CodingKeys: String, CodingKey {
+        case challengeId
+        case userNickName
+        case missionTitle
+        case receiptImageId
+        case status
+        case questImageId = "questImage"
+        case createdAt
+        case likeCnt
+        case hateCnt
+    }
     static let challengeMockData = Challenge(challengeId: "", userNickName: "일상유저123", missionTitle: "바닐라라떼 마시기", receiptImageId: "", status: "", questImageId: "", createdAt: "2024-01-01'T'00:00:00", likeCnt: 3, hateCnt: 0)
 }
 
