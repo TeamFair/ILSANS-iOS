b8fbf68754a8f6d47407899616800b6e3f83033f
radin <dlwlsgml8847@gmail.com>
2024-10-19 00:39:08 +0900

    [Ref/#172] 활동로그 목데이터 위치 이동

--- ILSANG/Sources/Views/MyPage/MyPageView.swift
+++ ILSANG/Sources/Views/MyPage/MyPageView.swift
@@ -9,12 +9,6 @@ import SwiftUI
 
 struct MyPageView: View {
     
-    @State var activeTestData: [XPContent] = [
-            XPContent(recordId: 1, title: "Mission Accomplished", xpPoint: 100, createDate: "2024-06-21"),
-            XPContent(recordId: 2, title: "Daily Login", xpPoint: 50, createDate: "2024-06-22"),
-            XPContent(recordId: 3, title: "Quest Completed", xpPoint: 150, createDate: "2024-06-23")
-        ]
-    
     @StateObject var vm: MypageViewModel = MypageViewModel(userNetwork: UserNetwork(), challengeNetwork: ChallengeNetwork(), imageNetwork: ImageNetwork(), xpNetwork: XPNetwork())
     
     @State var segmentSelect = 0
