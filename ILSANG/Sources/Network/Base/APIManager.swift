c55558db4a8a43be71a3320d9c8a8d009cff31fe
radin <dlwlsgml8847@gmail.com>
2024-10-31 14:22:42 +0900

    [Chore/#189] 빌드 환경설정 수정에 따른 APIManager 변경

--- ILSANG/Sources/Network/Base/APIManager.swift
+++ ILSANG/Sources/Network/Base/APIManager.swift
@@ -8,18 +8,47 @@
 import Foundation
 
 final class APIManager {
-    static let baseURL = "http://43.202.229.190"
-    
-    #if DEBUG
-    static let port = "9090"
-    #else
-    static let port = "9091"
-    #endif
+    static let baseURL = EnvironmentConfig.rootURL + ":" + EnvironmentConfig.port
     
     ///개발용 토큰
     static let authDevelopToken =    "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJhMTFjNDQxMS0xOTU0LTRiMWEtYjg0Ny0zYWY4NjU5MTNiM2YiLCJ1c2VyVHlwZSI6IkNVU1RPTUVSIn0.tM8UzimEvBVOH3hC-Put8J3iVU-QAeXlteRmcFjFHus"
     
     static func makeURL(_ target: APITarget) -> String {
-        baseURL + ":" + port + "/api/" + target.type + "/" + target.path
+        baseURL + "/api/" + target.type + "/" + target.path
     }
+}
+
+
+enum EnvironmentConfig {
+    // MARK: - Keys
+    enum Keys {
+        enum Plist {
+            static let rootURL = "ROOT_URL"
+            static let port = "PORT"
+        }
+    }
+
+    // MARK: - Plist
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("Plist file not found")
+        }
+        return dict
+    }()
+
+    // MARK: - Plist values
+    static let rootURL: String = {
+        guard let rootURLstring = EnvironmentConfig.infoDictionary[Keys.Plist.rootURL] as? String else {
+            fatalError("Root URL not set in plist for this environment")
+        }
+   
+        return rootURLstring
+    }()
+
+    static let port: String = {
+        guard let port = EnvironmentConfig.infoDictionary[Keys.Plist.port] as? String else {
+            fatalError("Port not set in plist for this environment")
+        }
+        return port
+    }()
 }
